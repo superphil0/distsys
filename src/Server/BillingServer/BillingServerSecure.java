7793e32a90c36e0dcbd786de04cb483e9776589f
Philipp <philippomenitsch@gmx.at>
2012-11-26 23:19:31 +0100

    pricesteps should work now

--- src/Server/BillingServer/BillingServerSecure.java
+++ src/Server/BillingServer/BillingServerSecure.java
@@ -1,21 +1,34 @@
 package Server.BillingServer;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 
 public class BillingServerSecure implements Remote{
+	
+	private PriceSteps priceSteps;
+	public BillingServerSecure() {
+		priceSteps = new PriceSteps();
+	}
 	PriceSteps getPriceSteps()
 	{
-		return null;
+		return priceSteps;
 		
 	}
-	void createPriceStep(double startPrice, double endPrice, double fixedPrice, double variablePricePercent)
+	void createPriceStep(double startPrice, double endPrice, double fixedPrice, double variablePricePercent) throws RemoteException
 	{
-		
+		PriceStep step = new PriceStep(startPrice, endPrice, fixedPrice, variablePricePercent);
+		if(!priceSteps.addPriceStep(step))
+		{
+			throw new RemoteException("Could not add PriceStep because it already exists or it is invalid: " + step);
+		}
 	}
-	void deletePriceStep(double startPrice, double endPrice)
+	void deletePriceStep(double startPrice, double endPrice) throws RemoteException
 	{
-		
+		if(!priceSteps.deletePriceStep(new PriceStep(startPrice, endPrice, 1, 1)))
+		{
+			throw new RemoteException("Could not delete PriceStep because invalid: " + startPrice + " endprice " + endPrice);
+		}
 	}
 	void billAuction(String user, long auctionID, double price)
 	{
--- src/Server/BillingServer/PriceSteps.java
+++ src/Server/BillingServer/PriceSteps.java
@@ -4,5 +4,48 @@ import java.util.LinkedList;
 
 public class PriceSteps {
 	private LinkedList<PriceStep> priceSteps;
-
+	
+	public PriceSteps()
+	{
+		this.priceSteps = new LinkedList<PriceStep>();
+	}
+	public LinkedList<PriceStep> getPriceSteps()
+	{
+		return priceSteps;
+	}
+	
+	boolean addPriceStep(PriceStep step)
+	{
+		if(priceSteps.contains(step) || !step.checkValid()) return false;
+		return true;
+	}
+	
+	boolean deletePriceStep(PriceStep step)
+	{
+		if(!priceSteps.contains(step))
+		{
+			return false;
+		}
+		
+		PriceStep step2 = priceSteps.get(priceSteps.indexOf(step));
+		if(step.getEndPrice() == step2.getEndPrice())
+		{
+			priceSteps.remove(step2);
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		java.util.Collections.sort(priceSteps);
+		String ret = "";
+		for(PriceStep step : priceSteps)
+		{
+			ret += step.toString() + "\n";
+		}
+		return ret;		
+	}
+	
+	
 }
