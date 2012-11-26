37b2847d479da77771c1b985ba9113e33b557bca
Daniela Koehle <daniela.koehle@student.tuwien.ac.at>
2012-11-26 01:15:13 +0100

    ManagementClient

---
+++ src/Server/ManagementClient.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import Events.Event;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author daniela
+ */
+public class ManagementClient implements IReceiveEvent {
+
+    public void receiveEvent(Event event) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
