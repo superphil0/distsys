b1f384fa29f60f5a434c0d4fab630fbef71075e8
Philipp <philippomenitsch@gmx.at>
2013-01-10 16:35:15 +0100

    stage 3 done working on 4 2

---
+++ src/Client/ClientLogic.java
@@ -0,0 +1,13 @@
+package Client;
+
+import java.net.InetAddress;
+
+public class ClientLogic {
+	
+	
+	public String getSignedBid(String bid, InetAddress client1, InetAddress client2)
+	{
+		return null;	
+	}
+
+}
---
+++ src/Client/TimeStampResponder.java
@@ -0,0 +1,40 @@
+package Client;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.PrivateKey;
+
+import Channel.Base64Channel;
+import Channel.SecureChannel;
+import Channel.TCPChannel;
+
+public class TimeStampResponder extends Thread{
+	
+	private static final String CHARSET = "UTF-8";
+	private Socket socket;
+	private PrivateKey key;
+	private SecureChannel secureChannel;
+	private BufferedReader in;
+	private PrintWriter out;
+	public TimeStampResponder(Socket accept, PrivateKey key) {
+		socket = accept;
+		this.key = key;
+	}
+	
+	@Override
+	public void run() {
+try {
+        	out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), CHARSET ), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream(), CHARSET));
+            secureChannel = new SecureChannel(new Base64Channel(new TCPChannel(out, in)));
+            secureChannel.setPrivKey(key);
+
+        } catch (IOException e) {
+            out.println("Problem with In or Output - Connection.");
+        }
+
+	}
+	
+
+}
