f4678213fd506dfc045b1b15935a9ce9fa142eeb
Daniela Koehle <daniela.koehle@student.tuwien.ac.at>
2013-01-10 16:15:32 +0100

    stage 1 + 2 for windows

--- src/Server/ServerStartUp.java
+++ src/Server/ServerStartUp.java
@@ -15,117 +15,135 @@ import java.io.InputStreamReader;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.bouncycastle.openssl.PEMReader;
 import org.bouncycastle.openssl.PasswordFinder;
 
 /**
  * Starts the AuctionServer and Shuts it down, if user hits enter
- *
+ * 
  * @author daniela
  */
 public class ServerStartUp {
 
-    private static AuctionServer auctionServer;
-    private static String analyticsBindingName, billingBindingName;
-    private static String pathToPrivKey, pathToClientKeyDir;
-    private static PrivateKey myPrivKey;
+	private static AuctionServer auctionServer;
+	private static String analyticsBindingName, billingBindingName;
+	private static String pathToPrivKey, pathToClientKeyDir;
+	private static PrivateKey myPrivKey;
 
-    public static void main(String[] args) throws IOException {
-        new RegistryProperties();
-        RegistryProperties.getPort();
-        RegistryProperties.getHost();
+	public static void main(String[] args) throws IOException {
+		new RegistryProperties();
+		RegistryProperties.getPort();
+		RegistryProperties.getHost();
 
-        int port = 0;
-        if (args.length != 5) {
-            System.out.println("Please enter the port, analyticsBindingName and"
-                    + "billingBindingName  as argument on which you want the server to run");
-            System.out.println(args.length);
-            System.exit(0);
-        }
-        if (args != null && !args[0].isEmpty()) {
-            try {
-                port = Integer.parseInt(args[0]);
-                analyticsBindingName = args[1];
-                billingBindingName = args[2];
-                pathToPrivKey = args[3];
-                pathToClientKeyDir = args[4];
-            } catch (NumberFormatException nfe) {
-                System.err.println("PortNumber has to be a number! E.g. Server <Port>");
-            }
-        } else {
-            System.err.println("Please enter a Port Number!");
-        }
+		int port = 0;
+		if (args.length != 5) {
+			System.out
+					.println("Please enter the port, analyticsBindingName and"
+							+ "billingBindingName  as argument on which you want the server to run");
+			System.out.println(args.length);
+			System.exit(0);
+		}
+		if (args != null && !args[0].isEmpty()) {
+			try {
+				port = Integer.parseInt(args[0]);
+				analyticsBindingName = args[1];
+				billingBindingName = args[2];
+				pathToPrivKey = args[3];
+				pathToClientKeyDir = args[4];
+			} catch (NumberFormatException nfe) {
+				System.err
+						.println("PortNumber has to be a number! E.g. Server <Port>");
+			}
+		} else {
+			System.err.println("Please enter a Port Number!");
+		}
 
-        for (int i = 0; i < 3; i++) {
-            try {
-                myPrivKey = getPrivateKey(pathToPrivKey);
-                if (myPrivKey != null) {
-                    System.out.println("Success!");
-                    break;
-                }
-            } catch (KeyNotFoundException ex) {
-                System.out.println(ex.getMessage());
-                //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (WrongPasswordException ex) {
-                System.out.println(ex.getMessage());
+		for (int i = 0; i < 3; i++) {
+			try {
+				myPrivKey = getPrivateKey(pathToPrivKey);
+				if (myPrivKey != null) {
+					System.out.println("Success!");
+					break;
+				}
+			} catch (KeyNotFoundException ex) {
+				System.out.println(ex.getMessage());
+				// Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE,
+				// null, ex);
+			} catch (WrongPasswordException ex) {
+				System.out.println(ex.getMessage());
 
-                //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            if (i == 2) {
-                System.out.println("Wrong Key, Access denied.");
-                return;
-            }
-        }
+				// Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE,
+				// null, ex);
+			}
+			if (i == 2) {
+				System.out.println("Wrong Key, Access denied.");
+				return;
+			}
+		}
 
-        if (port > 0) {
-            auctionServer = new AuctionServer(port, analyticsBindingName, billingBindingName, myPrivKey, pathToClientKeyDir);
-            auctionServer.start();
-        }
+		if (port > 0) {
+			auctionServer = new AuctionServer(port, analyticsBindingName,
+					billingBindingName, myPrivKey, pathToClientKeyDir);
+			auctionServer.start();
+		}
 
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("AuctionServer started, hit enter to shut down.");
+		System.out.println("AuctionServer started, hit enter to shut down.");
 
+		if (scanner.hasNextLine()) {
+			auctionServer.close();
+		}
+		System.out.println("Bye.");
 
-        if (scanner.hasNextLine()) {
-            auctionServer.close();
-        }
-        System.out.println("Bye.");
+	}
 
-    }
+	private static PrivateKey getPrivateKey(String pathToPrivKey)
+			throws KeyNotFoundException, WrongPasswordException {
+		PrivateKey privateKey = null;
+		PEMReader in;
 
-    private static PrivateKey getPrivateKey(String pathToPrivKey) throws KeyNotFoundException, WrongPasswordException {
-        PrivateKey privateKey = null;
-        PEMReader in;
-        try {
-            in = new PEMReader(new FileReader(pathToPrivKey), new PasswordFinder() {
-                public char[] getPassword() {
-                    // reads the password from standard input for decrypting the private key
-                    System.out.println("Enter pass phrase:");
-                    try {
-                        return new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
-                    } catch (IOException ex) {
-                        //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-                        return null;
-                    }
-                }
-            });
-            if (in == null) {
-                throw new KeyNotFoundException("Couldn't read Key");
-            }
-        } catch (FileNotFoundException ex) {
-            throw new KeyNotFoundException("Wrong path to Private Server Key");
-            //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        KeyPair keyPair;
-        try {
-            keyPair = (KeyPair) in.readObject();
-        } catch (IOException ex) {
-            throw new WrongPasswordException("Wrong Password, please try again!");
-            //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        privateKey = keyPair.getPrivate();
-        return privateKey;
+		try {
+			in = new PEMReader(new FileReader(pathToPrivKey),
+					new PasswordFinder() {
+						public char[] getPassword() {
+							// reads the password from standard input for
+							// decrypting the
+							// private key
+							System.out.println("Enter pass phrase:");
+							try {
+								char[] pw = new BufferedReader(
+										new InputStreamReader(System.in))
+										.readLine().toCharArray();
+								// System.out.println(">input pw: " + pw.toString());
+								return pw;
+							} catch (IOException ex) {
+								Logger.getLogger(ServerThread.class.getName())
+										.log(Level.SEVERE, null, ex);
+								return null;
+							}
+						}
+					});
+			KeyPair keyPair;
 
-    }
+			keyPair = (KeyPair) in.readObject();
+
+			privateKey = keyPair.getPrivate();
+			in.close();
+
+			return privateKey;
+
+		} catch (FileNotFoundException e) {
+			throw new KeyNotFoundException("Wrong path to Private Server Key");
+			// e.printStackTrace();
+		} catch (IOException e) {
+			throw new WrongPasswordException(
+					"Wrong Password, please try again!");
+			// e.printStackTrace();
+		} 
+
+	}
 }
